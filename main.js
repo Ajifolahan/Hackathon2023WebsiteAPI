ce495eeb879b79d03d0080eb766419f2bfd40522
Ajifolahan <momoreajifolahan@gmail.com>
2023-04-01 12:49:35 -0400

    Displayed IP details

--- main.js
+++ main.js
@@ -29,6 +29,14 @@ function getTime() {
         .then(response => {
             console.log(response)
              const resultElement = document.getElementById('result');
-        resultElement.innerHTML = `Datetime: ${response.day_of_year}`;})
+        resultElement.innerHTML = `Day Of the Year: ${response.day_of_year}`;
+        const resultElement1 = document.getElementById('result1');
+        resultElement1.innerHTML = `Date and Time: ${response.datetime}`;
+        const resultElement2 = document.getElementById('result2');
+        resultElement2.innerHTML = `Day Of the Week: ${response.day_of_week}`;
+        const resultElement3 = document.getElementById('result3');
+        resultElement3.innerHTML = `Timezone: ${response.timezone}`;
+        const resultElement4 = document.getElementById('result4');
+        resultElement4.innerHTML = `Week of the Year: ${response.week_number}`;})
         .catch(err => console.error(err));
 }
