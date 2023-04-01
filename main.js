3bcdadd7846e2ef9c298e6997f8e450bece4a1d5
Ajifolahan <momoreajifolahan@gmail.com>
2023-04-01 14:10:40 -0400

    finished demo

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
 function getTime() {
-    // const location = document.getElementById('city').value;
-    // const area = document.getElementById('area').value;
-    // const apiPass = `${location}/${area}`;
-
-    //const url = `https://world-time2.p.rapidapi.com/timezone/${apiPass}`;
-
     const options = {
         method: 'GET',
         headers: {
@@ -12,17 +6,7 @@ function getTime() {
             'X-RapidAPI-Host': 'world-time2.p.rapidapi.com'
         }
     };
-
-    // fetch(url, options)
-    // .then(response => response.json())
-    // .then(response => {
-    //     console.log(response)
-    //     // const resultElement = document.getElementById('result');
-    //     // resultElement.innerHTML = `Datetime: ${response.datetime}`;
-    // })
-    // .catch(err => console.error(err));
-
-    
+  
     
     fetch('https://world-time2.p.rapidapi.com/ip', options)
         .then(response => response.json())
